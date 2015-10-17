636f5559024e23218a21d9465d547bd4fd8ba747
David L. Bennett <db42@muku42.local>
2015-10-16 17:41:47 -1000

    remove extraneous whitespace

--- get_avatar.py
+++ get_avatar.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # COMMAND LINE PROGRAM TO DOWNLOAD A USER'S AVATAR FROM GITHUB.
-# USAGE: `PYTHON get_avatar.py <GITHUB_USERNAME>`. 
+# USAGE: `PYTHON get_avatar.py <GITHUB_USERNAME>`.
 
 import sys
 import json
@@ -10,11 +10,11 @@ import shutil
 
 # PARSE COMMAND LINE ARGUMENTS
 PARSER = argparse.ArgumentParser()
-PARSER.add_argument(  'username')
+PARSER.add_argument('username')
 ARGS = PARSER.parse_args()
 # CALL THE GITHUB API AND GET USER INFO
 RequestUrl = 'https://api.github.com/users/' + ARGS.username
-RESULT = requests.get( RequestUrl )
+RESULT = requests.get(RequestUrl)
 if RESULT.ok :
     user_info = json.loads(RESULT.content)
     avatarURL = user_info['avatar_url']
@@ -23,7 +23,7 @@ else:
                      exiting now, sorry...\n".format(ARGS.username))
     sys.exit()
 # DOWNLOAD AND SAVE IMAGE FILE
-I = requests.get(avatarURL , stream=True)
+I = requests.get(avatarURL, stream=True)
 if I.ok:
-    with open(ARGS.username + '.png' , 'wb') as OuTfIle:
-        shutil.copyfileobj( I.raw,  OuTfIle )
+    with open(ARGS.username + '.png', 'wb') as OuTfIle:
+        shutil.copyfileobj(I.raw, OuTfIle)
