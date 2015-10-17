2490481cfa230bf8e0e48f7e2a773f254da1f9bd
David L. Bennett <db42@muku42.local>
2015-10-16 14:45:52 -1000

    moved all imports together

--- get_avatar.py
+++ get_avatar.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # COMMAND LINE PROGRAM TO DOWNLOAD A USER'S AVATAR FROM GITHUB. USAGE: `PYTHON get_avatar.py <GITHUB_USERNAME>`. 
-import sys, json
-# PARSE COMMAND LINE ARGUMENTS
+
+import sys
+import json
 import argparse
+import requests
+import shutil
+
+# PARSE COMMAND LINE ARGUMENTS
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument(  'username')
 ARGS = PARSER.parse_args()
 # CALL THE GITHUB API AND GET USER INFO
 RequestUrl = 'https://api.github.com/users/' + ARGS.username
-import requests
 RESULT = requests.get( RequestUrl )
 if RESULT.ok :
     user_info = json.loads(RESULT.content)
@@ -19,6 +23,5 @@ else:
 # DOWNLOAD AND SAVE IMAGE FILE
 I = requests.get(avatarURL , stream=True)
 if I.ok:
-    import shutil
     with open(ARGS.username + '.png' , 'wb') as OuTfIle:
         shutil.copyfileobj( I.raw,  OuTfIle )
