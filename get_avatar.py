1956693e644653315dbc405d1e082f7b4c2d5d91
David L. Bennett <db42@muku42.local>
2015-10-16 18:06:15 -1000

    unified naming styles and changed I to imageFile

--- get_avatar.py
+++ get_avatar.py
@@ -9,21 +9,21 @@ import requests
 import shutil
 
 # PARSE COMMAND LINE ARGUMENTS
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument('username')
-ARGS = PARSER.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('username')
+args = parser.parse_args()
 # CALL THE GITHUB API AND GET USER INFO
-RequestUrl = 'https://api.github.com/users/' + ARGS.username
-RESULT = requests.get(RequestUrl)
-if RESULT.ok :
-    user_info = json.loads(RESULT.content)
+requestURL = 'https://api.github.com/users/' + args.username
+result = requests.get(requestURL)
+if result.ok :
+    user_info = json.loads(result.content)
     avatarURL = user_info['avatar_url']
 else:
     sys.stderr.write("Error fetching user information for {0};\
-                     exiting now, sorry...\n".format(ARGS.username))
+                     exiting now, sorry...\n".format(args.username))
     sys.exit()
 # DOWNLOAD AND SAVE IMAGE FILE
-I = requests.get(avatarURL, stream=True)
-if I.ok:
-    with open(ARGS.username + '.png', 'wb') as OuTfIle:
-        shutil.copyfileobj(I.raw, OuTfIle)
+imageFile = requests.get(avatarURL, stream=True)
+if imageFile.ok:
+    with open(args.username + '.png', 'wb') as outFile:
+        shutil.copyfileobj(imageFile.raw, outFile)
